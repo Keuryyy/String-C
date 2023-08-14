9bac5ed7c8058e5a7982b5052c7c1c138c0e07ad
Keuryyy <132664535+Keuryyy@users.noreply.github.com>
2023-08-14 10:20:26 -0300

    Update String1.C

--- String1.C
+++ String1.C
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
 
+int main(){
+
+    char frase[50];
+    int QuantidadeVogal = 0;
+    int i;
+
+    printf("Informe uma frase: ");
+    gets(frase);
+
+    for(i = 0; i < strlen(frase); i++){
+        if(tolower(frase[i]) == tolower('a') || tolower(frase[i]) == tolower('e') || tolower(frase[i]) == tolower('i') || tolower(frase[i]) == tolower('o') || tolower(frase[i]) == tolower('u')){
+            QuantidadeVogal++;
+        }
+    }
+
+    printf("Quantidade de vogais: %d\n", QuantidadeVogal);
+}
